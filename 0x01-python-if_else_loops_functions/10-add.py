44ec2b15aea4021ab7d2e8a6c796ba06c031512c
Nimona123 <argano4444@gmail.com>
2023-10-31 04:44:24 -0700

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 10-add.py
+# Nimona123 <argano4444@gmail.com>
+
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 11-pow.py
+# Nimona123
+
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)
