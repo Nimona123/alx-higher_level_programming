2addeb72bdb1824b264d9bd6354b0db01ddb415d
Nimona123 <134584118+Nimona123@users.noreply.github.com>
2024-03-26 22:00:08 +0300

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ const request = require('request');
 
 request.get(process.argv[2])
   .on('response', function (response) {
-  console.log('code:, ${response.statusCode}'); // Print the response status code if a response was received
+  console.log('code: ${response.statusCode}'); // Print the response status code if a response was received
 });
